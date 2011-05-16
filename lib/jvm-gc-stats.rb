320f3bbef7de39591f6e915f97a36370c67794b2
Ryan King <ryan@theryanking.com>
2011-05-16 14:27:24 -0700

    create gem

---
+++ lib/jvm-gc-stats.rb
@@ -0,0 +1,7 @@
+module Jvm
+  module Gc
+    module Stats
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jvm-gc-stats/version.rb
@@ -0,0 +1,7 @@
+module Jvm
+  module Gc
+    module Stats
+      VERSION = "0.0.1"
+    end
+  end
+end
